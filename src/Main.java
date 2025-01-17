3227a0efff1eca6ad1308c4dc438ba975d531b1a
RyunK <lrk9582@gmail.com>
2025-01-17 11:59:27 +0900

    김태원 2.대소문자 변환 해결 완료

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,33 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static int solution(String s, String c){
+    public String solution(String s){
 
-        s = s.toLowerCase();
-        c = c.toLowerCase();
+        StringBuilder ret_builder = new StringBuilder();
+        char temp;
 
-        int ret = 0;
+        for(int i=0; i<s.length();i++) {
+            temp = s.charAt(i);
 
-        while(s.contains(c)){
-            s = s.substring(s.indexOf(c)+1);
-            ret += 1;
+            if (Character.isUpperCase(temp)) {
+                temp = Character.toLowerCase(temp);
+                ret_builder.append(temp);
+            } else {
+                temp = Character.toUpperCase(temp);
+                ret_builder.append(temp);
+            }
         }
 
+        String ret = ret_builder.toString();
         return ret;
     }
 
-
     public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
-        String input1 = in.nextLine();
-        String input2 = in.next();
-        System.out.println(solution(input1, input2));
+        Main sol = new Main();
+        Scanner in=new Scanner(System.in);
+
+        String input1 = in.next();
+        System.out.println(sol.solution(input1));
         return ;
     }
 }
